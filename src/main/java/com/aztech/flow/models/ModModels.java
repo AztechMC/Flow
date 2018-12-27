95af1ba03f1772473017e379527bf8b88534f337
Bruno Ploumhans <bruno.ploumhans@gmail.com>
2018-12-27 10:46:40 +0100

    Add scroll texture

---
+++ src/main/java/com/aztech/flow/models/ModModels.java
@@ -0,0 +1,23 @@
+package com.aztech.flow.models;
+
+import com.aztech.flow.items.ModItems;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+@Mod.EventBusSubscriber(Side.CLIENT)
+public class ModModels {
+
+    @SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event) {
+        registerModel(ModItems.SCROLL);
+    }
+
+    private static void registerModel(Item item) {
+        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+    }
+}
